a8245696a6e952f7e2afc48981e5de1a04873d61
Mohamed Gamal <94301975+mohamedGamal90@users.noreply.github.com>
2024-12-10 15:12:28 +0200

    feat: Logout Handler (#32)

--- packages/api/src/apiClient.ts
+++ packages/api/src/apiClient.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { protectedStore, StoreKey } from "@aurora/utils";
+import { router } from "expo-router";
+import { deleteValue } from "@aurora/utils/src/protectedStore";
 
 export const getApiUrl = (url: string) => {
   return `${process.env.EXPO_PUBLIC_API_URL}${url}`;
@@ -21,6 +23,20 @@ authApiClient.interceptors.request.use(async config => {
   }
 
   return config;
+});
+
+authApiClient.interceptors.response.use(undefined, error => {
+  if (error?.response?.status) {
+    switch (error.response.status) {
+      case 401:
+        deleteValue(StoreKey.AccessToken);
+        router.push("auth/login");
+        break;
+      default:
+        break;
+    }
+  }
+  return Promise.reject(error);
 });
 
 export { apiClient, authApiClient };
