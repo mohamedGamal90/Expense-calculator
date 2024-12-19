8d3acd23655e32ece03ad1bffc6806d0d49ca96d
Ali Bassiouni <aabassiouni@hotmail.com>
2024-12-19 17:24:10 +0200

    fix: fix window crash on resize (#54)

--- packages/ui/blocks/src/CardList/component/CardListLoading.tsx
+++ packages/ui/blocks/src/CardList/component/CardListLoading.tsx
@@ -1,19 +1,21 @@
 import { useWindowDimensions, View } from "@aurora/components";
 
-const { width: screenWidth } = useWindowDimensions();
-export const CardListLoading = () => (
-  <View>
-    <View
-      width={screenWidth - 315}
-      height={200}
-      backgroundColor="$secondary100"
-      borderRadius="$sm"
-      marginHorizontal="$base"
-    />
-    <View flexDirection="row" justifyContent="center" gap="$xs" marginTop="$base">
-      {[...Array(3)].map((_, index) => (
-        <View key={index} width={8} height={8} borderRadius={4} backgroundColor="$secondary100" />
-      ))}
+export function CardListLoading() {
+  const { width: screenWidth } = useWindowDimensions();
+  return (
+    <View>
+      <View
+        width={screenWidth - 315}
+        height={200}
+        backgroundColor="$secondary100"
+        borderRadius="$sm"
+        marginHorizontal="$base"
+      />
+      <View flexDirection="row" justifyContent="center" gap="$xs" marginTop="$base">
+        {[...Array(3)].map((_, index) => (
+          <View key={index} width={8} height={8} borderRadius={4} backgroundColor="$secondary100" />
+        ))}
+      </View>
     </View>
-  </View>
-);
+  );
+}
