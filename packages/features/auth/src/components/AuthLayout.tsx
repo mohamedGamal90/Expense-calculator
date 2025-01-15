284bfa19a032d922e0d46b002bae63e6e13dfae5
Mohamed Gamal <94301975+mohamedGamal90@users.noreply.github.com>
2025-01-15 10:43:08 +0200

    fix: authlayout scroll (#117)

--- packages/features/auth/src/components/AuthLayout.tsx
+++ packages/features/auth/src/components/AuthLayout.tsx
@@ -3,7 +3,6 @@ import { RootView, ScrollView, useMedia, View } from "@aurora/components";
 // import { useSettingStore } from "@metroid/store";
 import { Image } from "expo-image";
 import { useRouter } from "expo-router";
-import { useCallback } from "react";
 
 // const items = [
 //   { label: "EN", value: "en" },
@@ -15,96 +14,79 @@ export function AuthLayout({ children }: { children: React.ReactNode }) {
   const media = useMedia();
   // const { lang } = useSettingStore();
 
-  const screen = useCallback(
-    () => (
-      <>
-        <View
-          $gtMd={{
-            w: "50%",
-            h: "100%",
-            py: "$base",
-          }}
-          w="100%">
-          {media.md ? (
-            <View w="100%" t={-10} h={70} borderRadius="$sm" bg="white" opacity={0.73} jc="center">
+  return (
+    <RootView
+      $gtMd={{
+        px: "$4xl",
+      }}
+      f={1}
+      fw="wrap"
+      bg="white"
+      fd="row-reverse">
+      <View
+        $gtMd={{
+          w: "50%",
+          h: "100%",
+          py: "$base",
+        }}
+        w="100%">
+        {media.md ? (
+          <View w="100%" t={-10} h={70} br="$sm" bg="white" opacity={0.73} jc="center">
+            <Image
+              source={require("./logo.png")}
+              style={{ width: "100%", height: "70%" }}
+              contentFit="contain"
+            />
+          </View>
+        ) : (
+          <Image
+            style={{
+              flex: 1,
+            }}
+            priority="high"
+            source={require("../assets/login-img.png")}
+            contentFit="contain"
+          />
+        )}
+      </View>
+      <View
+        $gtMd={{
+          w: "50%",
+          h: "100%",
+        }}
+        w="100%"
+        px="$m"
+        alignItems="center"
+        py="$base">
+        {media.gtMd && (
+          <View fd="row" jc="space-between" ai="center" mb="$xl" width="100%" py="$base" h={100}>
+            <View
+              cursor="pointer"
+              f={0.4}
+              h="100%"
+              alignSelf="flex-start"
+              onPress={() => router.push("/")}>
               <Image
                 source={require("./logo.png")}
-                style={{ width: "100%", height: "70%" }}
+                style={{ width: "100%", height: "100%" }}
                 contentFit="contain"
               />
             </View>
-          ) : (
-            <Image
-              style={{
-                flex: 1,
-              }}
-              priority="high"
-              source={require("../assets/login-img.png")}
-              contentFit="contain"
-            />
-          )}
-        </View>
-        <View
-          $gtMd={{
-            w: "50%",
-          }}
-          w="100%"
-          px="$m"
-          alignItems="center"
-          py="$base">
-          {media.gtMd && (
-            <View
-              fd="row"
-              jc="space-between"
-              alignItems="center"
-              mb="$xl"
-              width="100%"
-              py="$base"
-              h={100}>
-              <View
-                cursor="pointer"
-                f={0.4}
-                h="100%"
-                alignSelf="flex-start"
-                onPress={() => router.push("/")}>
-                <Image
-                  source={require("./logo.png")}
-                  style={{ width: "100%", height: "100%" }}
-                  contentFit="contain"
-                />
-              </View>
 
-              {/* <View flex={0.2}>
+            {/* <View flex={0.2}>
               <StyledSelect
                 items={items}
                 value={lang as string}
                 onSelect={value => changeLang(value as Lang)}
               />
             </View> */}
-            </View>
-          )}
+          </View>
+        )}
 
-          <ScrollView style={{ width: "100%", flex: 1, paddingRight: 13 }}>
-            <View gap="$5xl" jc="center">
-              {children}
-            </View>
-          </ScrollView>
-        </View>
-      </>
-    ),
-    [media, router, children],
-  );
-
-  return (
-    <RootView
-      $gtMd={{
-        paddingHorizontal: "$4xl",
-      }}
-      f={1}
-      fw="wrap"
-      bg="white"
-      fd="row-reverse">
-      {screen()}
+        <ScrollView style={{ width: "100%", flex: 1, paddingRight: 13 }}>
+          <View gap="$5xl">{children}</View>
+        </ScrollView>
+      </View>
     </RootView>
   );
 }
