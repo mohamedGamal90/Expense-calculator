3466f03844884916e3e067943595e35cc9b5775e
Mohamed Gamal <94301975+mohamedGamal90@users.noreply.github.com>
2025-01-20 15:49:51 +0200

    fix: cardnumber overlaping issue (MD-577) (#123)

--- packages/ui/blocks/src/CardList/component/CardItem.tsx
+++ packages/ui/blocks/src/CardList/component/CardItem.tsx
@@ -1,20 +1,16 @@
-import { memo } from "react";
 import { StyleSheet, ImageBackground as RNImageBackground } from "react-native";
 import { StyledText, View } from "@aurora/components";
 import { CardType } from "@metroid/types";
+import { memo } from "react";
 
-type Props = {
-  card: CardType;
-  width: number;
-};
-
+type Props = { card: CardType; width: number };
 export const CardItem = memo(({ card, width }: Props) => (
   <View w={width} h={200} ai="center" jc="center" px="$xl" alignSelf="center" mx="$auto">
     <RNImageBackground
       source={require("../../cardImage.png")}
       style={styles.imageBackground}
       resizeMode="contain">
-      <View bg="black" alignSelf="center" br={5} p={5} r={45} t={145}>
+      <View bg="black" alignSelf="center" br={5} p={5} r={55} t={155}>
         <StyledText variant="Headingl" col={"white"}>
           Ending with ** {card.cardMask.slice(-4)}
         </StyledText>
